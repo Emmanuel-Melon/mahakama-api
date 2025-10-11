1b0973717e7523738a435a70f54e02acdbd4e888
= <emmanuelgatwech@gmail.com>
2025-10-11 14:19:26 +0300

    feat: seed documents

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { userRoutes } from "../users/user.routes";
-import documentRoutes from "../documents/document.routes";
-import lawyerRoutes from "../lawyers/lawyer.routes";
-import questionRoutes from "../questions/question.routes";
+import userRouter from "../users/user.routes";
+import documentRouter from "../documents/document.routes";
+import lawyerRouter from "../lawyers/lawyer.routes";
+import questionRouter from "../questions/question.routes";
 const router = Router();
 
-router.use("/users", userRoutes);
-router.use("/lawyers", lawyerRoutes);
-router.use("/questions", questionRoutes);
-router.use("/documents", documentRoutes);
+router.use("/users", userRouter);
+router.use("/lawyers", lawyerRouter);
+router.use("/questions", questionRouter);
+router.use("/documents", documentRouter);
 
 export default router;
